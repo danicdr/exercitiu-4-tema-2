ffbd3bb33fe9fa8d33bb85cb26fc6f518caf0f74
Daniel Codreanu <danielcdr2006@gmail.com>
2023-04-01 23:54:45 +0300

    Add project files.

---
+++ exercitiu 4 tema 2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace MyFirstProgram2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int i = 0;
+
+            int temp = n;
+            while (temp != 0)
+            {
+                int c = temp % 10;
+                i = i * 10 + c;
+                temp = temp / 10;
+            }
+
+            if (n == i)
+            {
+                Console.WriteLine("Palindrom");
+            }
+            else
+            {
+                Console.WriteLine("Nu e palindrom");
+            }
+        }
+    }
+}
